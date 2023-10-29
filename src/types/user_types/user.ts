7c6f3a7059d44f44d495a3406578e48c80c84045
Animesh <animeshece1998@gmail.com>
2023-10-29 17:21:12 +0530

    User Model Created

---
+++ src/types/user_types/user.ts
@@ -0,0 +1,14 @@
+type User = {
+    id: string
+    username: string
+    email: string
+    fname: string
+    lname: string
+    dob?: Date
+    phone?: string
+    image?: string
+    is_active: boolean
+    account_type: string
+    created_at: string
+    updated_at: string
+};
