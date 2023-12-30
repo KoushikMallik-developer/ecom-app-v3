ec62a9eb56bbd05fa17bbfdbdafffd8d646305aa
Animesh <animeshece1998@gmail.com>
2023-12-30 14:12:13 +0530

    404 error page done

--- src/components/404_error/page_not_found.tsx
+++ src/components/404_error/page_not_found.tsx
@@ -1,77 +1,35 @@
-import React, { useEffect } from 'react';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import errorImage from "../../components/404_error/404.png";
 
-const NotFoundPage: React.FC = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = '/src/components/404_error/js/404.js';
-    script.async = true;
-    document.body.appendChild(script);
-
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = '/src/components/404_error/css/404.css';
-    document.head.appendChild(link);
-  }, []);
-
+const NotFoundPage = () => {
   return (
-    <>
-    <html lang="en">
-      <head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-1752YPLP7H"
-        ></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-            gtag("config", "G-1752YPLP7H");
-          `}
-        </script>
-
-        <title>Space | tsParticles 404</title>
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="../images/tsParticles-64.png"
-        />
-      </head>
-
-      <body className="permission_denied">
-        <div id="tsparticles"></div>
-        <div className="denied__wrapper">
-          <h1>404</h1>
-          <h3>
-            LOST IN <span>SPACE</span> Shoppixa? Hmm, looks like that page
-            doesn't exist.
-          </h3>
-          <img
-            id="astronaut"
-            src="/src/components/404_error/images/astronaut.svg"
-            alt="Astronaut"
+    <MDBContainer className="py-5">
+      <MDBRow className="justify-content-center align-items-center min-vh-100">
+        <MDBCol md="6" className="text-center">
+          <MDBCardImage
+            src={errorImage}
+            alt="Error 404"
+            className="img-fluid mb-3"
+            style={{ maxWidth: "300px" }}
           />
-          <img
-            id="planet"
-            src="/src/components/404_error/images/planet.svg"
-            alt="Planet"
-          />
-          <a href="/">
-            <button className="denied__link">Go Home</button>
-          </a>
-        </div>
-
-        <script
-          type="text/javascript"
-          src="https://cdn.jsdelivr.net/npm/tsparticles@2/tsparticles.bundle.min.js"
-        ></script>
-      </body>
-    </html>
-    </>
+          <MDBBtn
+            href="/"
+            color="primary"
+            className="d-flex align-items-center justify-content-center"
+          >
+            <MDBIcon icon="home" className="me-2" />
+            Dashboard
+          </MDBBtn>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
